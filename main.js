83e25cd8e1328c6001bd3e895b8105d48081d808
cikay <cikay@iMac.local>
2018-09-02 16:46:00 +0900

    sitemap.xml

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var sm = require('sitemap');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var http = require('http');
@@ -8,19 +9,21 @@ var url = require('url');
 var qs = require('querystring');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-
-// favicon
 var favicon = require('serve-favicon');
-app.use(favicon(__dirname + '/public/image/favicon.png'));
-
 // For security
 var helmet = require('helmet');
-app.use(helmet());
-// compression
-app.use(compression());
 // require routes
 var indexRouter = require('./routes/index');
 var trackingRouter = require('./routes/tracking');
+
+// favicon
+app.use(favicon(__dirname + '/public/image/favicon.png'));
+
+// For security
+app.use(helmet());
+// compression
+app.use(compression());
+
 // static files
 app.use(express.static('public'));
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
@@ -40,6 +43,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/tracking', trackingRouter);
 
+// sitemap
+var sitemap = sm.createSitemap ({
+    hostname: 'http://www.trackingpkg.com',
+    cacheTime: 600000,        // 600 sec - cache purge period
+    urls: [
+      { url: '/',                 changefreq: 'daily', priority: 0.7 },
+      { url: '/tracking/result',  changefreq: 'always', priority: 0.3 }
+    ]
+  });
+app.get('/sitemap.xml', function(req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.send( sitemap.toString() );
+});
 
 // 404
 app.use(function(req, res, next) {
